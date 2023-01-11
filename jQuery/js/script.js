d8eac467bf68f88df6c6c9aad2c081e63347f880
David Říháček <davidrihacekk@gmail.com>
2023-01-11 19:23:45 +0100

    jQuery

---
+++ jQuery/js/script.js
@@ -0,0 +1,3 @@
+$(function(){
+    $(".first-box").fadeOut(3000);
+})
--- photoGalleryProject/script.js
+++ photoGalleryProject/script.js
@@ -2,13 +2,13 @@ let slides = document.querySelectorAll(".slide");
 
 const deleteActiveAll = function(){
     slides.forEach(function(singleSlide){
-        singleSlide.classList.remove("active")
+        singleSlide.classList.add("active")
     })
 }
 
 slides.forEach(function(singleSlide){
     singleSlide.addEventListener("click", function(){
         deleteActiveAll()
-        singleSlide.classList.add("active")
+        singleSlide.classList.remove("active")
     })
 })
